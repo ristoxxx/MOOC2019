31aa813ea9ec07d8addea5f9be5dfbc478c14215
Risto Lähteenkorva <56062215+ristoxxx@users.noreply.github.com>
2020-03-08 21:13:07 +0200

    Add files via upload

---
+++ osa01-Osa01_04.OlipaKerranMaa/src/main/java/OlipaKerranMaa.java
@@ -0,0 +1,11 @@
+
+public class OlipaKerranMaa {
+
+    public static void main(String[] args) {
+        // Toteuta ohjelmasi tänne
+        System.out.println("Olipa kerran maa valmistui vuonna 2008. Sarja käsittelee\n"
+                + "luontoympäristön suojelemista ja varoittaa maailmanlaajuisesta\n"
+                + "ilmastonlämpenemisestä, kasvihuoneilmiöstä, saasteista ja\n"
+                + "niin edelleen.");
+    }
+}
