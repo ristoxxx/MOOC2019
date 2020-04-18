b1bb5c86039b2322ef47c71f4dd213cc124e019b
ristoxxx <lahteri@gmail.com>
2020-04-18 21:07:37 +0300

    eka versio

---
+++ dayOfYear/src/yksikkotestaus/DayOfYear.java
@@ -0,0 +1,57 @@
+package yksikkotestaus;
+
+/**
+ * Smelly Example #1
+ * 
+ * https://web.mit.edu/6.005/www/fa16/classes/04-code-review/
+ * 
+ * Collaboratively authored with contributions from: Saman Amarasinghe, Adam
+ * Chlipala, Srini Devadas, Michael Ernst, Max Goldman, John Guttag, Daniel
+ * Jackson, Rob Miller, Martin Rinard, and Armando Solar-Lezama.
+ * 
+ * This work is licensed under CC BY-SA 4.0.
+ */
+public class DayOfYear {
+	
+	
+    /**
+     * Tämä konstruktori on merkitty yksityiseksi, koska luokka on "utility class",
+     * eli se sisältää ainoastaan staattisia metodeja. Tästä luokasta ei ole
+     * sallittua luoda olioita, vaan metodeja tulee kutsua luokan kautta.
+     * 
+     * Katso esim:
+     * 
+     * @see http://www.javapractices.com/topic/TopicAction.do?Id=40
+     * @see https://rules.sonarsource.com/java/tag/design/RSPEC-1118
+     */
+    private DayOfYear() {
+    }
+
+    public static int dayOfYear(int month, int dayOfMonth, int year) {
+    	if ((month < 0 || month > 12) || (year < 1901 || year > 2099)) {
+    		return -1; //If input value is out of range, -1 will be returned. WARNING!!! does not check day of month
+    		//throw new IllegalArgumentException("One or more of values are out of range");
+    	} 
+    	int Jan,Mar,May,Jul,Aug,Oct,Dec ;//initialize number of days in month 
+    		Jan=Mar=May=Jul=Aug=Oct=Dec = 31;
+    	int Apr,Jun,Sep,Nov;
+    		Apr=Jun=Sep=Nov= 30;
+    	int Feb = 28;
+    	if (year % 4 == 0) {//checking if it's leap year. WARNING!!! his is accurate only between years 1901-2099 
+    		Feb += 1;//adding leap day to total number of days if it's leap year
+    	}
+    	int Zero = 0;//Just to make list easier to understand. First value of list will be zero so that months[1] will be Jan. 
+    	int[] months = {Zero,Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec};//Make list of months 
+    	int days = dayOfMonth;//Separate number of day from input number for calculating purpose 
+    	/*if month is something else than January, adding number of days of each month one by one
+    	to total number of days until number of months is reached*/
+    	if (month > 1) { 
+    		int i = 0;  
+    		while (i < month) {
+    			days += months[i];
+				i++;
+			}
+    	}
+    	return days;
+    }
+}
---
+++ dayOfYear/src/yksikkotestaus/DayOfYearTest.java
@@ -0,0 +1,45 @@
+package yksikkotestaus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Month;
+
+import org.junit.jupiter.api.Test;
+
+class DayOfYearTest {
+
+	//@Test
+	//void test() {
+	//	fail("Not yet implemented");
+	//}
+	@Test
+	//Is the result for first day of the year number 1
+	public void eka() {
+		int x = DayOfYear.dayOfYear(1, 1, 2020);
+		assertEquals(1, x);
+	}
+	@Test
+	//Does 1. of march give correct result if leap year
+	public void toka() {
+		int x = DayOfYear.dayOfYear(3, 1, 2020);
+		assertEquals(61, x);
+	}
+	@Test
+	//Does 1. of march give correct result if not leap year
+	public void kolmas() {
+		int x = DayOfYear.dayOfYear(3, 1, 2019);
+		assertEquals(60, x);
+	}
+	@Test
+	//Does last day of year give correct result if not leap year
+	public void neljas() {
+		int x = DayOfYear.dayOfYear(12, 31, 2019);
+		assertEquals(365, x);
+	}
+	@Test
+	//If value out of range, does it return -1
+	public void viides() {
+		int x = DayOfYear.dayOfYear(19, 31, 2019);
+		assertEquals(-1, x);
+	}
+}
